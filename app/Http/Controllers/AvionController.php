80fc6f5e127a6b496d278b621f6843d30d729672
aalonso <ayee.alonsoo@gmail.com>
2019-12-27 13:59:10 -0300

    crud con api

--- app/Http/Controllers/AvionController.php
+++ app/Http/Controllers/AvionController.php
@@ -5,37 +5,29 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
-class AvionController extends Controller {
+// Necesitaremos el modelo Avion para ciertas tareas.
+use App\Avion;
 
+class AvionController extends Controller {
     /**
      * Display a listing of the resource.
      *
      * @return Response
      */
+
     public function index()
     {
-        //
+        // Devolverá todos los fabricantes.
+        // return "Mostrando todos los fabricantes de la base de datos.";
+        // return Fabricante::all();  No es lo más correcto por que se devolverían todos los registros. Se recomienda usar Filtros.
+        // Se debería devolver un objeto con una propiedad como mínimo data y el array de resultados en esa propiedad.
+        // A su vez también es necesario devolver el código HTTP de la respuesta.
+        // php http://elbauldelprogramador.com/buenas-practicas-para-el-diseno-de-una-api-RESTful-pragmatica/
+        // https://cloud.google.com/storage/docs/json_api/v1/status-codes
+
+        return response()->json(['status'=>'ok','data'=>Avion::all()], 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @return Response
-     */
-    public function store()
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -46,39 +38,21 @@ class AvionController extends Controller {
     public function show($id)
     {
         //
-    }
+        // return "Se muestra Fabricante con id: $id";
+        // Buscamos un fabricante por el id.
+        $avion=Avion::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+        // Si no existe ese avion devolvemos un error.
+        if (!$avion)
+        {
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
+            // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+            return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra un avión con ese código.'])],404);
+        }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function update($id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
+        return response()->json(['status'=>'ok','data'=>$avion],200);
     }
 
 }
+
+// Se han borrado el resto de métodos que no se necesitan.
